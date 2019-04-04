4d6429063ec7ce3d4ba013363e5e9c7f64227956
rndso15 <rndso15@naver.com>
2019-04-04 15:13:14 +0900

    release_02

--- app/src/main/java/com/example/capstone_release_01/MainActivity.java
+++ app/src/main/java/com/example/capstone_release_01/MainActivity.java
@@ -1,13 +1,96 @@
 package com.example.capstone_release_01;
 
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    RecognitionListener listener;
+    SpeechRecognizer mRecognizer;
+    Intent intent;
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        final Button button = (Button) findViewById(R.id.button);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE , getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE , "ko-KR");
+
+        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mRecognizer.setRecognitionListener(listener);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRecognizer.startListening(intent);
+            }
+        });
+
+        listener = new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                String key = "";
+                key = SpeechRecognizer.RESULTS_RECOGNITION;
+                ArrayList<String> mResult = results.getStringArrayList(key);
+                String[] rs = new String[mResult.size()];
+                mResult.toArray(rs);
+                textView.setText(""+rs[0]);
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        };
     }
 }
