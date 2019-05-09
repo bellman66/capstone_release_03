dacbde438922690aa812d1b8d9dda4b5f837ecc1
rndso15 <rndso15@naver.com>
2019-05-09 16:26:16 +0900

    release_03

--- app/src/main/java/com/example/capstone_release_01/FileList.java
+++ app/src/main/java/com/example/capstone_release_01/FileList.java
@@ -1,9 +1,16 @@
 package com.example.capstone_release_01;
 
+import android.Manifest;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -12,22 +19,38 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 // 파일 리스트 관리 - 리스트 엑티비티.
 public class FileList extends ListActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1111;
+    private int permissioncheck ;
 
     private ArrayList<String> mylist;   // file 내 list.
     private File selected_file; // 선택되는 file.
 
     private String state;
     private TextView tv;
-
+    private File dir;
+    private File file;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        permissioncheck = ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if(permissioncheck == PackageManager.PERMISSION_DENIED){
+            // 권한 없음
+           // ActivityCompat.requestPermissions(get, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+
+        }
+        else {
+            // 권한 있음
+        }
 
         // 객체 구현.
         mylist = new ArrayList<String>();
@@ -36,9 +59,46 @@ public class FileList extends ListActivity {
         // 1. 어떤 발표문을 가져와서 확인하는지 해당 폴더를 가져오는 역할.
 
         // #########    외부저장소가 지정 , 저장이 안되는 상황.
-        String pathname = Environment.getExternalStorageDirectory().getAbsolutePath();
-        File path = Environment.getExternalStoragePublicDirectory( Environment.DIRECTORY_DOWNLOADS + "/capstone");
-        File file = new File( path ,"capstone");
+        String pathname = Environment.getExternalStorageDirectory().getAbsolutePath().toString();
+        pathname += "/Capstone";
+
+        File dir = new File(pathname);
+        dir.mkdir();
+
+        file = new File(dir , "file.txt");
+
+        try {
+            FileOutputStream fos= new FileOutputStream(file);
+            String str = " 이 파일은 sd카드에 저장된 메세지";
+            fos.write(str.getBytes());
+            fos.close();
+
+        } catch (FileNotFoundException e) {
+            showMsg("지정된 파일을 생성할 수 없습니다.");
+        } catch (IOException e) {
+            showMsg("파일에 데이터를 쓸 수 없습니다.");
+        }
+
+
+            /*
+            if(!file.exists()){
+                Toast.makeText(getApplicationContext(),"존재하긴함" , Toast.LENGTH_LONG).show();
+
+                if(!file.mkdirs()){
+                    Toast.makeText(getApplicationContext(), " 생성 x", Toast.LENGTH_LONG).show();
+                }
+            }*/
+
+            pathname += "/First_text.txt";
+
+        file = new File(pathname);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         // ########
 
         // 1-1../capstone 폴더가 존재하지 않으면 mkdir을 이용해서 폴더생성.
@@ -53,15 +113,6 @@ public class FileList extends ListActivity {
             }
         }
         */
-
-        // ############# 생성이 안되는 문제점 발견 ########################
-        if(!file.exists()){
-                    Toast.makeText(getApplicationContext(),"존재하긴함" , Toast.LENGTH_LONG).show();
-
-                    if(!file.mkdir()){
-                        Toast.makeText(getApplicationContext(), " 생성 x", Toast.LENGTH_LONG).show();
-            }
-        }
 
         // 파일내 list 를 가져옴.
        /*
@@ -78,6 +129,32 @@ public class FileList extends ListActivity {
         setListAdapter(Adapter);
         */
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] permissions,int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS:
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 권한 허가
+                    // 해당 권한을 사용해서 작업을 진행할 수 있습니다
+                } else {
+                    // 권한 거부
+                    // 사용자가 해당권한을 거부했을때 해주어야 할 동작을 수행합니다
+                }
+                return;
+        }
+    }
+
+
+    private void showMsg(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+
 
     /*
     public void Get_list() {
@@ -134,28 +211,9 @@ public class FileList extends ListActivity {
         return 1;
     }
     */
-    /*
-    boolean checkExternalStorage() {
-        state = Environment.getExternalStorageState();
-        // 외부메모리 상태
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            // 읽기 쓰기 모두 가능
-            Toast.makeText(getApplicationContext() , " 외부 메모리 읽기 쓰기 가능 ", Toast.LENGTH_LONG).show();;
-            Log.d("test", "외부메모리 읽기 쓰기 모두 가능");
-            tv.setText("외부메모리 읽기 쓰기 모두 가능");
-            return true;
-        } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)){
-            //읽기전용
-            Toast.makeText(getApplicationContext() , " 외부 메모리 가능 ", Toast.LENGTH_LONG).show();;
-            Log.d("test", "외부메모리 읽기만 가능");
-            tv.setText("외부메모리 읽기만 가능");
-            return false;
-        } else {
-            // 읽기쓰기 모두 안됨
-            Toast.makeText(getApplicationContext() , " 외부 메모리 둘다 안됨.ㅂ ", Toast.LENGTH_LONG).show();;
-            Log.d("test", "외부메모리 읽기쓰기 모두 안됨 : "+ state);
-            tv.setText("외부메모리 읽기쓰기 모두 안됨 : "+ state);
-            return false;
-        }
-    }*/
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
 }
